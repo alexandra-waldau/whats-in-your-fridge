8c7ce274fe8869ad6feeee17bc1af085b408c770
Alexandra Waldau <alew@itu.dk>
2020-07-10 16:46:55 +0200

    updated ReadMe

---
+++ empty-fridge/build/precache-manifest.887161297093d3be89ee65199d4273d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9c1c24c7135a3bf7916cf5a82b1bbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8aef64e9c6d562b54bb",
+    "url": "/static/css/main.e87d4c7c.chunk.css"
+  },
+  {
+    "revision": "0c0463f04c3eecf03eb1",
+    "url": "/static/js/2.7972fae9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7972fae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8aef64e9c6d562b54bb",
+    "url": "/static/js/main.81442e72.chunk.js"
+  },
+  {
+    "revision": "910c11cad6095b50b8d8",
+    "url": "/static/js/runtime-main.5f8c770c.js"
+  }
+]);
